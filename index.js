87bb89c90f34f3c0cec2fab97add9fd9c56f6d79
unknown <18OJohnston.acc@lwetb.ie>
2022-12-01 11:22:33 +0000

    bob

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 function calculateSquare(length) {
-    return {
-        area: length**2,
-        perimiter: length*4
-    }
+    return `The area is: ${length**2}, The perimiter is: ${length*4}`
+}
+
+function calculateRect(length, width) {
+    return `The area is: ${length*width}, The perimiter is: ${length*2+width*2}`
+}
+
+function calculateSimpleInterest(P, N, R) {
+    R /= 100
+    return `The Simple Interest is: ${(P*N*R)}`
+}
+
+let map = {
+    square: calculateSquare,
+    rectangle: calculateRect,
+    interest: calculateSimpleInterest,
+}
+
+function getElement(id) {
+    return document.getElementById(id).value
+}
+
+function parseSquare() {
+    var length = getElement("squarelength")
+    alert(calculateSquare(length))
+}
+
+function parseRect() {
+    var length = getElement("rectlength")
+    var height = getElement("heightlength")
+    alert(calculateRect(length, height))
+}
+
+function parseSimpleInterest() {
+    var P = getElement("princamount")
+    var N = getElement("time")
+    var R = getElement("annualrate")
+    alert(calculateSimpleInterest(P,N,R))
 }
 
 
